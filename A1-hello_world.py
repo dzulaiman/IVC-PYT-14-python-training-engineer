03a86f78b9346b21c95b2f7b208f4ad587662728
dzulaiman <dzul.aiman@gmail.com>
2023-10-09 08:41:44 +0800

    Create A1-hello_world.py

---
+++ A1-hello_world.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#Hello World
+#dzul.aiman@gmail.com
+#2023-10-09
+
+print("Hello, World!")
+
+a = "Hello"
+type(a)
+
+b = "World"
+type(b)
+
+c = 2023
+type(c)
+
+d = [2020,2021,2022]
+print(d)
+
+print("\n")
+
+name = input("What is your name:")
+print("Welcome,",name)
