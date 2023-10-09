0ce0806b48e833f2d748797f95b38888a04ea25e
dzulaiman <dzul.aiman@gmail.com>
2023-10-09 10:46:11 +0800

    Create A3-arithmetic.py

---
+++ A3-arithmetic.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#Arithmetic python
+#dzul.aiman@gmail.com
+#2023-10-09
+
+# Arithmetic operators
+a = 10
+b = 5
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+exponentiation = a ** b
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Exponentiation:", exponentiation)
+
+
+# Modulus and floor division
+a = 10
+b = 3
+modulus_result = a % b  # Modulus operator returns the remainder of the division
+floor_division_result = a // b  # Floor division operator returns the quotient rounded down to the nearest integer
+
+print("Modulus Result:", modulus_result)
+print("Floor Division Result:", floor_division_result)
+
+
+# Assignment operators
+x = 5
+x += 3  # Equivalent to x = x + 3
+print("Updated x:", x)
+
+y = 10
+y **= 2  # Equivalent to y = y ** 2
+print("Updated y:", y)
