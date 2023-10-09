30dfc3aa0a1eb6d2615b884a14f8161e227fabbf
dzulaiman <dzul.aiman@gmail.com>
2023-10-09 11:44:56 +0800

    Create A4-modules-function.py

---
+++ A4-modules-function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#Training: function and modules
+#dzul.aiman@gmail.com
+#2023-10-09
+
+# Function definition
+def greet(name):
+    """This function greets the person passed in as a parameter."""
+    print("Hello, " + name + "!")
+
+# Function call
+greet("Alice")
+
+
+# Content of my_module.py
+def add(a, b):
+    """This function adds two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """This function subtracts two numbers."""
+    return a - b
