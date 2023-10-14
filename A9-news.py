0f57c639af83894291884e34d996eee32d14a35b
dzulaiman <dzul.aiman@gmail.com>
2023-10-14 15:04:13 +0800

    Create A9-news.py

---
+++ A9-news.py
@@ -0,0 +1,20 @@
+#Selenium test
+#dzul.aiman
+#2023-10-14
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+time.sleep(10)
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+time.sleep(10)
+driver.close()
